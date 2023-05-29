c4c4e51535354973dc1700a81899f54fa7c0b1b1
aruizmarin <andresrumarin@gmail.com>
2023-05-29 09:02:09 +0200

    Updated bootstrap.py dependencies versions

--- bootstrap.py
+++ bootstrap.py
@@ -70,10 +70,10 @@ parser.add_option("--allow-site-packages",
                   action="store_true", default=False,
                   help=("Let bootstrap.py use existing site packages"))
 parser.add_option("--buildout-version",
-                  default='2.5.3',
+                  default='3.0.1',
                   help="Use a specific zc.buildout version")
 parser.add_option("--setuptools-version",
-                  default='44.1.1',
+                  default='51.3.3',
                   help="Use a specific setuptools version")
 parser.add_option("--setuptools-to-dir",
                   default=eggsdir,
