984b2d80d20e098071a35e253a7fbff93db217ae
ARuizma <andresrumarin@gmail.com>
2023-06-09 12:10:46 +0200

    Added script file to automatically remove the Python3 incompatibilities in untracked files

---
+++ Remove_Python3_Incompatibilities.py
@@ -0,0 +1,35 @@
+# op_tests remove decode
+# Read in the file
+with open("bin/op_tests", 'r') as file:
+    filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace(".decode('utf-8')", '')
+
+# Write the file out again
+with open("bin/op_tests", 'w') as file:
+    file.write(filedata)
+
+# openprocurement_client/utils.py substitute basestring for str
+# Read in the file
+with open("src/openprocurement_client/openprocurement_client/utils.py", 'r') as file:
+    filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace("basestring", 'str')
+
+# Write the file out again
+with open("src/openprocurement_client/openprocurement_client/utils.py", 'w') as file:
+    file.write(filedata)
+
+# src/openprocurement_client/openprocurement_client/resources/tenders.py substitute basestring for str
+# Read in the file
+with open("src/openprocurement_client/openprocurement_client/resources/tenders.py", 'r') as file:
+    filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace("basestring", 'str')
+
+# Write the file out again
+with open("src/openprocurement_client/openprocurement_client/resources/tenders.py", 'w') as file:
+    file.write(filedata)
