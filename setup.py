9ce275a0d409e59089d882f5aea3f6c86f58979f
ARuizma <andresrumarin@gmail.com>
2023-06-23 10:45:43 +0200

    Changes to use selenium 4.9.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='op_robot_tests',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
-          'urllib3[socks] = 1.26'
+          'urllib3[socks] == 1.26.16'
           'selenium == 4.9.0',
           'Faker',
           'Pillow',
