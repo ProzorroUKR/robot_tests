8d05003be2b465ea56e44a08283060879d0d925c
ARuizma <andresrumarin@gmail.com>
2023-07-03 15:43:38 +0200

    Modify setup.py to avoid compatibility issues with changes in buildout.cfg

--- setup.py
+++ setup.py
@@ -18,8 +18,6 @@ setup(name='op_robot_tests',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
-          'urllib3[socks] == 1.26.16',
-          'selenium == 4.9.0',
           'Faker',
           'Pillow',
           'PyYAML',
