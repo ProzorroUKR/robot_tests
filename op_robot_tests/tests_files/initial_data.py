4fcd83b61df27d39caa177fbc22b090303a25536
VTinkovan <tslavikus@gmail.com>
2023-07-05 17:16:49 +0200

    added waiter in Поставити ставку updated convert_amount

--- op_robot_tests/tests_files/initial_data.py
+++ op_robot_tests/tests_files/initial_data.py
@@ -174,7 +174,8 @@ def translate_country_en(country):
 
 
 def convert_amount(amount):
-    return ("{0:.2f}".format(float(amount))).replace(',', ' ').replace('.', ',')
+    value = float('{:.2f}'.format(amount))
+    return ("{0:,}".format(float(value))).replace(',', ' ').replace('.', ',')
     # return ("{:,}".format(float(amount))).replace(',', ' ').replace('.', ',')
 
 
